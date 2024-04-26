f42b192c6043ce322f8f10ab3efc54417e40580b
Barış Onur Yıldız <86011311+barisonuryy@users.noreply.github.com>
2024-04-27 00:52:45 +0300

    feat:added second scene

--- Assets/Scripts/Object/couriMovement.cs
+++ Assets/Scripts/Object/couriMovement.cs
@@ -7,24 +7,50 @@ public class couriMovement : MonoBehaviour
     // Start is called before the first frame update
     public float speed = 0.8f;
     public float range = 3f;
-    float startingX;
+    float startingX,startingY;
     int dir = 1;
+    private bool isYCourie;
    public bool canTurn;
     void Start()
     {
         startingX = transform.position.x;
+        startingY = transform.position.y;
+        if (gameObject.name == "yCourie")
+        {
+            isYCourie = true;
+        }
+        else
+        {
+            isYCourie = false;
+        }
+        Debug.Log(gameObject.name);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector2.down * speed * Time.deltaTime * dir);
-        if (transform.position.x < startingX || transform.position.x > range + startingX)
+        
+        if (!isYCourie)
         {
-            canTurn= true;
-            dir *= -1;
+            transform.Translate(Vector2.down * speed * Time.deltaTime * dir);
+            if (transform.position.x < startingX || transform.position.x > range + startingX)
+            {
+                canTurn= true;
+                dir *= -1;
 
+            }
         }
+        else
+        {
+            transform.Translate(Vector2.left * speed * Time.deltaTime * dir);
+            if (transform.position.y < startingY || transform.position.y > range + startingY)
+            {
+                canTurn= true;
+                dir *= -1;
+
+            }
+        }
+       
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
