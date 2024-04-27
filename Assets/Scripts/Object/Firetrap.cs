d7c4756507dbcbeaab57bc3cc160d0ea1370ffb2
Skeior <talhakarasu@icloud.com>
2024-04-28 02:52:40 +0300

    feat: added fire trap
    added fire trap and second scene design updated

---
+++ Assets/Scripts/Object/Firetrap.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered; 
+    private bool active; 
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if (!triggered)
+                StartCoroutine(ActivateFiretrap());
+            if (active)
+                collision.GetComponent<PlayerHealth>().TakeDamage(damage);
+        }
+    }
+    private IEnumerator ActivateFiretrap()
+    {
+       
+        triggered = true;
+        spriteRend.color = Color.red; 
+        yield return new WaitForSeconds(activationDelay);
+        active = true;
+        anim.SetBool("activated", true);
+        spriteRend.color = Color.white;
+
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+}
