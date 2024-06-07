4c80a0efd9cd24e4082b64834438a91500fe9450
Skeior <talhakarasu@icloud.com>
2024-06-07 06:00:27 +0300

    feat: Level5 base design completed

---
+++ Assets/wall.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    bool isStarted;
+
+    void Start()
+    {
+        isStarted = true;
+    }
+
+    IEnumerator OpenTile(int b)
+    {
+        isStarted = false;
+
+        for (int i = 0; i < gameObject.transform.GetChild(b).childCount; i++)
+        {
+            gameObject.transform.GetChild(b).GetChild(i).gameObject.SetActive(true);
+            StartCoroutine(gameObject.GetComponent<FadeInFadeOut>()
+                .FadeIn(gameObject.transform.GetChild(b).GetChild(i).gameObject.GetComponent<SpriteRenderer>()));
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (isStarted && Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("Talha");
+            StartCoroutine(HandleKeyInput());
+        }
+    }
+
+    IEnumerator HandleKeyInput()
+    {
+        while (true)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Debug.Log("Talha1");
+                StartCoroutine(OpenTile(1));
+                break;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Debug.Log("Talha2");
+                StartCoroutine(OpenTile(2));
+                break;
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Debug.Log("Talha3");
+                StartCoroutine(OpenTile(3));
+                break;
+            }
+            yield return null;
+        }
+    }
+}
